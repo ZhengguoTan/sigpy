2e465d34a70be963d65873cb1230ea587cf875e3
Zhengguo Tan <zhengguo.tan@gmail.com>
2026-07-16 18:01:29 -0400

    test examples for meco models and recons

--- sigpy/mri/nlop.py
+++ sigpy/mri/nlop.py
@@ -9,14 +9,22 @@ This module contains these non-linear operators:
     * Diffusion,
         exponential diffusion modelling and parallel imaging sampling.
 
-Author: Zhengguo Tan <zhengguo.tan@gmail.com>
+    * MGRE,
+        multi gradient echoes.
+
+Author:
+    * 2022-2026 Zhengguo Tan <zhengguo.tan@gmail.com>
 """
+import math
+import numpy as np
 import sigpy as sp
 
-from sigpy import nlop
+from sigpy import backend, nlop
 from sigpy.mri import linop
 
+from typing import List, Literal
 
+# %%
 class Nlinv(sp.nlop.Nlop):
     """
     Construction of the non-linear parallel imaging (nlinv) operator.
@@ -152,7 +160,7 @@ class Nlinv(sp.nlop.Nlop):
             return output
 
 
-# class Diffusion(sp.nlop.Nlop):
+# %% class Diffusion(sp.nlop.Nlop):
 def Diffusion(input_shape, diff_enc, coil,
               scale=None, rvc=False, dwi_phase=None,
               coord=None, weights=None):
@@ -197,5 +205,263 @@ def Diffusion(input_shape, diff_enc, coil,
     # Compose (i.e. Chain) Sense linop with Diffusion nlop
     A = E * I * P
     A.repr_str = 'Diffusion'
+
+    return A
+
+# %%
+def calc_fat_modu(TE,  # second
+                  GYRO: float = 42.57747892E6,  # Hz/T
+                  B0: float = 0.55  # T
+                  ):
+    """
+    calculate fat modulation
+
+    Input:
+        TE - echo time in second
+        GYRO - Gyromagnetic ratio
+        B0 - Magnetic field strength
+    """
+    device = backend.get_device(TE)
+    xp = device.xp
+
+    with device:
+
+        TE = TE.ravel()
+
+        ppm = xp.array([-3.80, -3.40, -2.60, -1.94, -0.39, +0.60]) * 1E-6
+        # amp = xp.array([0.087, 0.693, 0.128, 0.004, 0.039, 0.048])
+        amp = xp.array([0.086, 0.537, 0.165, 0.046, 0.052, 0.114])
+
+        res = xp.zeros([len(TE), 2], dtype=complex)
+
+        for n in range(len(TE)):
+
+            for p in range(len(ppm)):
+
+                res[n, 1] += amp[p] * xp.exp(2j * math.pi * GYRO * B0 * ppm[p] * TE[n])
+
+        res[:, 0] = 1.
+
+    return res
+
+class WFZ(sp.nlop.Nlop):
+    """
+    Construction of the non-linear operator WFZ
+
+    Given the unknown x = (W, F, Z)^T, where
+        W: water
+        F: fat
+        Z: fB0 * 2 * pi + R2*
+    and the encoding array encode (usually the echo times t),
+
+    the forward operation is
+        F_t (x) = (W + F * fm_t) * exp(encode_t * Z)
+
+    fm_t is the fat-modulated phases along echo times
+
+    Args:
+        ishape (tuple): input shape
+        encode (array): echo times in milli-second (ms)
+        scale (list): TODO
+
+    References:
+        Tan Z., Unterberg-Buchwald C., Blumenthal M., et al. (2023).
+        Free-breathin liver fat, R2* and B0 field mapping
+        using multi-echo radial FLASH and
+        regularized model-based reconstruction.
+        IEEE Trans. Med. Imaging, 42, 1374-1387.
+    """
+    def __init__(self, ishape, encode,
+                 B0: float = 0.55,  # Tesla
+                 scale: List[float] = None,
+                 rvc: List[bool] = None,
+                 device=backend.Device(-1),
+                 repr_str: str = None):
+        image_shape = list(ishape[1:])
+        self.num_param = ishape[0]
+
+        # encode: [num_echo, 1]
+        self.num_echo = encode.shape[0]
+        assert encode.shape[1] == 1
+
+        oshape = [self.num_echo] + image_shape
+
+        self.encode = backend.to_device(encode, device)
+        self.device = device
+
+        self.fm = calc_fat_modu(self.encode * 1E-3, B0=B0)  # second
+
+        print('   > fm:')
+        print(self.fm)
+
+
+        if scale is None:
+            scale = [1.] * self.num_param
+        else:
+            assert self.num_param == len(scale)
+
+        if rvc is None:
+            self.rvc = [False] * self.num_param
+        else:
+            if len(rvc) == 1:
+                self.rvc = list(rvc) * self.num_param
+            else:
+                self.rvc = list(rvc)
+                assert self.num_param == len(self.rvc)
+
+        super().__init__(oshape, ishape, scale=scale, repr_str=repr_str)
+
+    def _get_params(self, x):
+
+        assert backend.get_device(x) == self.device
+
+        with self.device:
+
+            Wat = x[0, ...]
+            Fat = x[1, ...]
+            fB0 = x[2, ...]
+
+        return Wat, Fat, fB0
+
+    def _forward(self, input):
+
+        assert self.device == backend.get_device(input)
+        assert self.num_param == input.shape[0]
+
+        xp = self.device.xp
+        with self.device:
+
+            self.x = input
+
+            for p in range(self.num_param):
+                self.x[p] *= self.scale[p]
+
+            Wat, Fat, fB0 = self._get_params(self.x)
+
+            output = xp.zeros(self.oshape, dtype=complex)
+            for e in range(self.num_echo):
+
+                output[e] = (self.fm[e, 0] * Wat + self.fm[e, 1] * Fat) *\
+                    xp.exp(2j * xp.pi * fB0 * self.encode[e, 0])
+
+        return output
+
+    def _get_Jacobian(self, x):
+
+        assert self.device == backend.get_device(x)
+
+        Jshape = [self.num_echo] + list(self.x.shape)
+
+        xp = self.device.xp
+        with self.device:
+
+            self.x = x
+            Wat, Fat, fB0 = self._get_params(self.x)
+
+            output = xp.zeros(Jshape, dtype=complex)
+
+            for e in range(self.num_echo):
+                phase_fB0 = xp.exp(2j * xp.pi * fB0 * self.encode[e, 0])
+
+                # Jacobian for Water
+                output[e, 0, ...] = phase_fB0
+
+                # Jacobian for Fat
+                output[e, 1, ...] = self.fm[e, 1] * phase_fB0
+
+                # Jacobian for fB0
+                output[e, 2, ...] = (self.fm[e, 0] * Wat + self.fm[e, 1] * Fat) *\
+                    phase_fB0 * (2j * xp.pi * self.encode[e, 0])
+
+        return output
+
+    def _derivative(self, x, dx):
+
+        assert self.device == backend.get_device(x)
+        assert self.device == backend.get_device(dx)
+
+        xp = self.device.xp
+        with self.device:
+            self.Jacobian = self._get_Jacobian(x)
+            # print('   > Jacobian shape: ', self.Jacobian.shape)
+            return xp.sum(self.Jacobian * dx, axis=1)
+
+    def _adjoint(self, x, dy):
+
+        assert self.device == backend.get_device(x)
+        assert self.device == backend.get_device(dy)
+
+        xp = self.device.xp
+        with self.device:
+            self.Jacobian = self._get_Jacobian(x)
+            JH = xp.conjugate(xp.moveaxis(self.Jacobian, 0, 1))
+            dx = xp.sum(JH * dy, axis=1)
+
+            for e in range(self.num_param):
+                if self.rvc[e] is True:
+                    dx[e, ...] = dx[e, ...].real + 0. * 1j
+
+            return dx
+
+
+def MGRE(input_shape, TE, coil,
+         model_waterfat: bool = False,
+         B0: float = 0.55,
+         trafos: Literal['sobolev', 'identity'] = 'identity',
+         scale=None, rvc=False,
+         coord=None, weights=None):
+    """
+    Model Multi-Echo Gradient Echo Signal
+    """
+    device = sp.backend.get_device(coil)
+    TE = sp.to_device(TE, device=device)
+
+    N_param = 3 if model_waterfat is True else 2
+    assert N_param == input_shape[0]
+
+    if trafos == 'identity':
+
+        single_shape = [1] + list(input_shape[1:])
+
+        scaling_ops = []
+        for p in range(N_param):
+            scaling = 1  # if p < N_param-1 else 1E-3
+            scaling_ops.append(sp.linop.Multiply(single_shape, scaling))
+
+        T = sp.linop.Diag(scaling_ops, iaxis=0, oaxis=0)
+
+    elif trafos == 'sobolev':
+
+        excl_fB0_shape = [N_param-1] + list(input_shape[1:])
+        incl_fB0_shape = [1] + list(input_shape[1:])
+
+        op1 = sp.linop.Identity(excl_fB0_shape)
+        op2 = sp.linop.Sobolev(incl_fB0_shape, a=11, b=2)
+
+        T = sp.linop.Diag([op1, op2], iaxis=0, oaxis=0)
+
+    print(' -> T ishape: ', T.ishape, ' oshape: ', T.oshape)
+
+
+    if model_waterfat is False:
+
+        P = nlop.Exponential(input_shape, TE,
+                             const_a=False, rvc=rvc, scale=scale)
+
+    else:
+
+        P = WFZ(input_shape, TE, scale=scale, rvc=rvc, device=device)
+
+    print(' -> P ishape: ', P.ishape, ' oshape: ', P.oshape)
+
+    # Parallel Imaging
+    E = linop.Sense(coil, ishape=P.oshape, coord=coord, weights=weights)
+
+    print(' -> E ishape: ', E.ishape, ' oshape: ', E.oshape)
+
+    # Compose (i.e. Chain) Sense linop with B0 nlop
+    A = E * P * T
+    A.repr_str = 'MGRE'
+    A.trafos = T
 
     return A
--- tests/mri/test_nlop.py
+++ tests/mri/test_nlop.py
@@ -16,6 +16,41 @@ if config.cupy_enabled:
 
 class TestNlop(unittest.TestCase):
 
+    def check_nlop_derivative(self, A,
+                              device=backend.cpu_device,
+                              dtype=float):
+        device = backend.Device(device)
+
+        scale = 1e-8
+        x = util.randn(A.ishape, dtype=dtype, device=device)
+        h = util.randn(A.ishape, dtype=dtype, device=device)
+
+        with device:
+            dy1 = (A.forward(x + scale * h) - A.forward(x)) / scale
+            dy2 = A.derivative(x, h)
+
+            npt.assert_allclose(backend.to_device(dy1),
+                                backend.to_device(dy2),
+                                atol=1e-5, rtol=1e-5,
+                                err_msg=A.repr_str + ' derivative operator!')
+
+    def check_nlop_adjoint(self, A, device=backend.cpu_device, dtype=float):
+        device = backend.Device(device)
+        x = util.randn(A.ishape, dtype=dtype, device=device)
+
+        dx = util.randn(A.ishape, dtype=dtype, device=device)
+        dy = util.randn(A.oshape, dtype=dtype, device=device)
+
+        xp = device.xp
+        with device:
+            lhs = xp.vdot(A.derivative(x, dx), dy)
+            rhs = xp.vdot(dx, A.adjoint(x, dy))
+
+            npt.assert_allclose(backend.to_device(lhs),
+                                backend.to_device(rhs),
+                                atol=1e-5, rtol=1e-5,
+                                err_msg=A.repr_str + ' adjoint operator!')
+
     def test_nlinv_model(self):
         for device in devices:
             xp = device.xp
@@ -90,3 +125,65 @@ class TestNlop(unittest.TestCase):
             npt.assert_allclose(backend.to_device(y),
                                 backend.to_device(y2),
                                 err_msg='Diffusion model mismatch!')
+
+    def test_WFZ_model(self):
+        for device in devices:
+            xp = device.xp
+
+            # Exponential
+            tvec = util.randn((5, 1), dtype=float, device=device)
+            N_echo = tvec.shape[0]
+
+            Wat = util.randn((1, 1, 3, 3), dtype=complex, device=device)
+            Fat = util.randn((1, 1, 3, 3), dtype=complex, device=device)
+            fB0 = util.randn((1, 1, 3, 3), dtype=complex, device=device)
+            x = xp.concatenate((Wat, Fat, fB0))
+
+            Model = nlop.WFZ(x.shape, tvec, device=device)
+            y1 = Model(x)
+
+            # test forward
+            fm = nlop.calc_fat_modu(tvec * 1E-3)
+            y2 = xp.zeros(Model.oshape, dtype=complex)
+
+            for e in range(tvec.shape[0]):
+                y2[e] = (fm[e, 0] * Wat + fm[e, 1] * Fat) *\
+                    xp.exp(2j * xp.pi * fB0 * tvec[e, 0])
+
+            npt.assert_allclose(backend.to_device(y1), 
+                                backend.to_device(y2), 
+                                err_msg='WFZ model mismatch!')
+
+            # test derivative
+            self.check_nlop_derivative(Model, device=device, dtype=x.dtype)
+
+            # test adjoint
+            self.check_nlop_adjoint(Model, device=device, dtype=x.dtype)
+
+
+    # def test_MGRE_model(self):
+    #     for device in devices:
+    #         xp = device.xp
+
+    #         # Exponential
+    #         tvec = util.randn((5, 1), dtype=float, device=device)
+    #         M0 = util.randn((1, 1, 3, 3), dtype=complex, device=device)
+    #         B0 = util.randn((1, 1, 3, 3), dtype=complex, device=device)
+    #         x = xp.concatenate((M0, B0))
+
+    #         Dr = xp.reshape(B0, (B0.shape[0], -1))
+    #         y1 = xp.exp(xp.matmul(tvec, Dr))
+    #         y1 = xp.reshape(y1, (5, 1, 3, 3))
+    #         y1 *= M0
+
+    #         # Sense
+    #         coils = util.randn((8, 3, 3), dtype=complex, device=device)
+
+    #         y2 = fourier.fft(coils * y1, axes=(-2, -1))
+
+    #         A = nlop.MGRE(x.shape, tvec, coils)
+    #         y = A(x)
+
+    #         npt.assert_allclose(backend.to_device(y),
+    #                             backend.to_device(y2),
+    #                             err_msg='B0Field model mismatch!')
